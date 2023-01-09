25a52a9c4ac137dc4069c8ea6af0df73851193a0
Ali Nurcahya Astawijaya <alinurcahya01@gmail.com>
2023-01-09 15:36:23 +0700

    ProgramInput2 Project

---
+++ ProgramInput2/ProgramInput2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProgramInput2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat variabel kosong
+            string nama;
+            int umur;
+
+            Console.WriteLine("=== PROGRAM INPUT 2 ===");
+            Console.Write("Masukan nama: ");
+            nama = Console.ReadLine();
+            Console.Write("Masukan alamat: ");
+            var alamat = Console.ReadLine();
+            Console.Write("Masukan umur: ");
+            umur = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Terimakasih!");
+            Console.WriteLine("Data Berikut");
+            Console.WriteLine("Nama: {0}", nama);
+            Console.WriteLine("Alamat: {0}", alamat);
+            Console.WriteLine("Umur: {0} tahun", umur);
+            Console.WriteLine("SUDAH DISIMPAN!");
+            Console.ReadLine();
+        }
+    }
+}
