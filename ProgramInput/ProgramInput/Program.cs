62e4b54a9a66be7fb5af581dba08fd35a0802528
Ali Nurcahya Astawijaya <alinurcahya01@gmail.com>
2023-01-09 15:22:00 +0700

    ProgramInput Project

---
+++ ProgramInput/ProgramInput/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProgramInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Menampilkan teks dengan enter
+            Console.WriteLine("=== PROGRAM INPUT ===");
+
+            // Menampilkan teks tanpa enter
+            Console.Write("Tuliskan nama kamu: ");
+
+            // Untuk menyimpan teks yang diinputkan
+            string nama = Console.ReadLine();
+
+            // Menampilkan teks dari variabel yang telah diinputkan
+            Console.WriteLine("Hi, "+ nama + " selamat datang!");
+
+            // Menampilkan ketikan keyboard sampai ketika di ketik kembali program akan tertutup
+            Console.ReadKey();
+        }
+    }
+}
