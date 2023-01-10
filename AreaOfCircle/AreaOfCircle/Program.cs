579247e10c9e7f9b772cbff1fd76e601d1a45471
Ali Nurcahya Astawijaya <alinurcahya01@gmail.com>
2023-01-10 11:17:16 +0700

    AreaOfCircle Project

---
+++ AreaOfCircle/AreaOfCircle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float Phi = 3.14f;
+
+            Console.WriteLine("== PROGRAM LUAS LINGKARAN ==");
+            Console.Write("Input jari-jari: ");
+            int r = int.Parse(Console.ReadLine());
+
+            var luas = Phi * r * r;
+
+            Console.WriteLine("Luas Lingkaran = {0}", luas);
+            Console.ReadLine();
+        }
+    }
+}
